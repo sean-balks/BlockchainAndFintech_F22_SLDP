2b7ee2af67d4802ce683d9b023358b0ce56488f9
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-11-05 15:21:32 -0400

    added query to find rows post eth merge

--- shanks/opensea.sql
+++ shanks/opensea.sql
@@ -1,6 +1,17 @@
 -- find the most recent trades on opensea in the last 24 hours 
 select * 
 from opensea.trades
-WHERE block_time > now() - interval '24 hours' AND amount_usd IS NOT NULL
+WHERE block_time > now() - interval '24 hours' 
+AND amount_usd IS NOT NULL
 ;
+
+-- find the most recent trades on opensea in the last 44 days (time since Sept 22nd) that is ethereum 
+select * 
+from opensea.trades
+WHERE block_time > now() - interval '44 days'
+AND amount_usd is NOT NULL
+AND blockchain == 'ethereum'
+limit 100
+;
+
 
