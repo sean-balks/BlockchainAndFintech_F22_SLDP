23cef71853fd48f348347e764723b5ac567c5f09
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-11-05 15:24:19 -0400

    added query to find rows post eth merge on nft trades

--- shanks/nft.sql
+++ shanks/nft.sql
@@ -3,3 +3,12 @@ select *
 from nft.trades
 WHERE block_time > now() - interval '24 hours' AND amount_usd IS NOT NULL
 ;
+
+-- find the most recent nft trades in the last 44 days (time since Sept 22nd) that is ethereum 
+select * 
+from nft.trades
+WHERE block_time > now() - interval '44 days'
+AND amount_usd is NOT NULL
+AND blockchain == 'ethereum'
+limit 100
+;
