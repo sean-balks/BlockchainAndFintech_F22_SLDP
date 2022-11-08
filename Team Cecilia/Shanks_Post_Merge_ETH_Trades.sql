a8437811e096c0b7eab7c68f1a16b07c727b6e29
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-11-08 17:08:55 -0500

    changed names

--- Team Cecilia/Shanks_Post_Merge_ETH_Trades.sql
+++ Team Cecilia/Shanks_Post_Merge_ETH_Trades.sql
@@ -1,10 +1,3 @@
--- find the most recent trades on opensea in the last 24 hours 
-select * 
-from opensea.trades
-WHERE block_time > now() - interval '24 hours' 
-AND amount_usd IS NOT NULL
-;
-
 -- find the most recent trades on opensea in the last 44 days (time since Sept 22nd) that is ethereum 
 select * 
 from opensea.trades
