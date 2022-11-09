a9d6043aa7cb7e4de30431e4e1f47656271c124a
ceciliayiyue <chenyiyuecyy@gmail.com>
2022-11-09 01:16:15 -0500

    nft

---
+++ Team Cecilia/Cecilia_TotalNFTVolume_NON_ETH.sql
@@ -0,0 +1,30 @@
+-- Run in Ethereum engine
+-- from 2022/06/15 to now
+WITH nft_vol_noneth as (
+SELECT 
+'Before ETH Merge' as merged_type,
+date_trunc('day', block_time) as day,
+    SUM(usd_amount) as usd_amount
+FROM 
+(SELECT * FROM nft.trades where original_currency!= 'ETH' AND original_currency!= 'WETH') as non_eth
+WHERE block_time  >= '2022-06-15 00:00'
+        and block_time <= '2022-09-15 00:00'
+AND buyer != seller
+GROUP BY day 
+
+union all
+
+SELECT 
+'After ETH Merge' as merged_type,
+date_trunc('day', block_time) as day,
+    SUM(usd_amount) as usd_amount
+FROM 
+(SELECT * FROM nft.trades where original_currency!= 'ETH' AND original_currency!= 'WETH') as non_eth
+WHERE block_time  >= '2022-09-14 00:00'
+AND buyer != seller
+GROUP BY day 
+
+ORDER BY day
+
+)
+select * from nft_vol_noneth
