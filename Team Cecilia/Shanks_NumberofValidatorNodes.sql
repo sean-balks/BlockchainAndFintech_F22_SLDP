a42867cc00c8e8619afaced5e28929a121be5911
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-11-09 13:26:18 -0500

    changed number of validators

--- Team Cecilia/Shanks_NumberofValidatorNodes.sql
+++ Team Cecilia/Shanks_NumberofValidatorNodes.sql
@@ -1,6 +1,9 @@
 -- Only works if you are on the Ethereum engine
-SELECT (SUM(value)/1e18)/32 AS total_staking_nodes
+SELECT date_trunc('day',block_time) AS day, ( SUM(value) / 1e18 )/32 AS total_staking_nodes 
 FROM ethereum.traces
 WHERE "to" = '\x00000000219ab540356cBB839Cbe05303d7705Fa'
 AND success
 AND value > 0
+AND block_time > '2022-09-15'
+group by day
+;
