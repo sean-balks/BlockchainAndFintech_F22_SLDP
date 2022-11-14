7ed6cac2fd3ba96c20e242f50c8fd20c5a4db459
izuo1 <117047536+izuo1@users.noreply.github.com>
2022-11-14 12:52:39 -0500

    Update Chenny_TotalValueDepositedInETH.sql

--- Team_Chenny/Chenny_TotalValueDepositedInETH.sql
+++ Team_Chenny/Chenny_TotalValueDepositedInETH.sql
@@ -1,5 +1,11 @@
-SELECT SUM(value)/1e18 AS total_deposited_eth
-FROM ethereum.traces
-WHERE "to" = '\x00000000219ab540356cBB839Cbe05303d7705Fa'
-AND success
-AND value > 0
+-- select sum(value) from ethereum.transactions where 
+-- block_time > '2020-10-14'
+-- and `to`=lower('0x00000000219ab540356cBB839Cbe05303d7705Fa')
+with deposit as (
+    select sum(value)/1e18 as total_deposit_eth ,
+           count( distinct `from`) as total_depositors
+    from ethereum.traces
+    where block_time > '2020-10-14'
+    and `to`=lower('0x00000000219ab540356cBB839Cbe05303d7705Fa')
+)
+select * from deposit
