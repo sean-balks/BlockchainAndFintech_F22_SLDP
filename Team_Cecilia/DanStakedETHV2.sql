944a18c38b0241fcd857fa584f20fff63894ae84
dpz1707 <90921421+dpz1707@users.noreply.github.com>
2022-11-15 01:53:59 -0500

    Update DanStakedETHV2.sql

--- Team_Cecilia/DanStakedETHV2.sql
+++ Team_Cecilia/DanStakedETHV2.sql
@@ -7,7 +7,7 @@ AND `to`=lower('0x00000000219ab540356cBB839Cbe05303d7705Fa')
 AND success
 
 
--- Query 2, staked eth pre merge. Takes about ? seconds to run
+-- Query 2, staked eth pre merge. Takes about 4 minutes to run
 SELECT (SUM(value)/1e18) AS staked_eth
 FROM ethereum.traces
 WHERE block_time <= '2022-09-15 00:00'
