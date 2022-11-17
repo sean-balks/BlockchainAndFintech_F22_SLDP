52af72e4a01be1e51e485600f64c63dac0a8bcfc
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-11-16 21:25:42 -0500

    small edits

--- Team_Cecilia/Shanks_Total_Staked_Nodes_Over_Time.sql
+++ Team_Cecilia/Shanks_Total_Staked_Nodes_Over_Time.sql
@@ -1,14 +1,14 @@
 
 -- Deposit is the total_staking_nodes over the time period post merge
 -- avg price is the average price of ethereum over the time period post merge
-with deposit as (
+with staking_nodes as (
     select date_trunc('day', block_time) as day, (sum(value)/1e18)/32 as total_staking_nodes
     from ethereum.traces
     where block_time >= '2022-09-15'
     and `to`=lower('0x00000000219ab540356cBB839Cbe05303d7705Fa')
     group by day
-), 
-avg_price as (
+),
+eth_price as (
     SELECT date_trunc('day', minute) as day
     , AVG(price)
     FROM prices.usd
@@ -16,4 +16,4 @@ avg_price as (
     AND minute > '2022-09-15'
     GROUP BY day
     )
-select * from deposit natural join avg_price
+select * from staking_nodes natural join eth_price
